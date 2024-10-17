273573c58299606eef0c68fb8890fed88af6d0e4
David Rojas <indior.dev@gmail.com>
2024-10-17 10:12:20 -0300

    Se modificó el _layout
    se implementó un header más estilizado

--- app/_layout.js
+++ app/_layout.js
@@ -1,28 +1,48 @@
 import { Stack } from 'expo-router';
+import { StyleSheet, Image } from 'react-native';
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: "#004080" },
+        headerTintColor: "white",
+        headerLeft: () => (
+          <Image
+            source={require('../assets/Logo/logo.png')}
+            style={{ width: 50, height: 50 }}
+          />
+        )
+      }}
+    >
       <Stack.Screen
         name="index"
-        options={{ title: 'Inicio de sesión' }} 
+        options={{ title: 'Inicio de sesión' }}
       />
       <Stack.Screen
         name="subs/index"
-        options={{ title: 'Tus suscripciones' }} 
+        options={{ title: 'Tus suscripciones' }}
       />
       <Stack.Screen
         name="user/index"
-        options={{ title: 'Tu perfil' }} 
+        options={{ title: 'Tu perfil' }}
       />
       <Stack.Screen
         name="user/edit_user"
-        options={{ title: 'Editar perfil' }} 
+        options={{ title: 'Editar perfil' }}
       />
       <Stack.Screen
         name="subs/new_sub"
-        options={{ title: 'Crear nueva suscripción' }} 
+        options={{ title: 'Crear nueva suscripción' }}
       />
+
     </Stack>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+})
