1ac6b49225082e60d4dc04a7d2ca5760ed2360e2
Bcofrex <cofrebenja321@gmail.com>
2024-11-16 14:49:25 -0300

    SDK and prettier fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,30 @@
-// https://docs.expo.dev/guides/using-eslint/
-module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier"],
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
   rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        "singleQuote": true,
-        "parser": "flow"
-      }
-    ]
-  }
-}
+    "prettier/prettier": "warn",
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": "warn",
+    "no-console": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    singleQuote: true, // Usa comillas simples
+    semi: true, // Usa punto y coma
+    trailingComma: "es5", // Comas al final en objetos y arrays, pero no en funciones
+    bracketSpacing: true, // Espaciado dentro de llaves
+    jsxSingleQuote: false, // Comillas dobles en JSX
+    tabWidth: 2, // Indentación de 2 espacios
+    arrowParens: "avoid", // Evita paréntesis en funciones de una sola línea
+};
+  
